d3f123b38f51da9de535d05babea8678460de925
Christian Bell <camaribell@yahoo.com>
2023-03-19 20:10:26 -0700

    done

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
+    let response = 
+    console.log(books)
 
     books.forEach(renderBook)
 }
